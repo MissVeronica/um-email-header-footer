08d429b83f046d77fd0de8b3134c7fe3b8cfa3b3
MissVeronica <veronica@tranza.se>
2024-03-14 17:18:28 +0100

    Update um-email-header-footer.php

--- um-email-header-footer.php
+++ um-email-header-footer.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name:     Ultimate Member - Email Header/Footer
  * Description:     Extension to Ultimate Member for adding HTML Header/Footer to all outgoing UM Notification emails.
- * Version:         1.2.0
+ * Version:         1.3.0
  * Requires PHP:    7.4
  * Author:          Miss Veronica
  * License:         GPL v2 or later
@@ -21,40 +21,51 @@ class UM_Email_Header_Footer {
     function __construct( ) {
 
         add_filter( 'um_settings_structure',         array( $this, 'um_settings_structure_header_footer' ), 10, 1 );
-        add_filter( 'um_email_send_message_content', array( $this, 'email_add_footer' ), 10, 3 );
-        add_filter( 'um_email_template_body_attrs',  array( $this, 'email_add_header' ), 10, 3 );
+        add_filter( 'um_email_send_message_content', array( $this, 'email_add_header_footer' ), 10, 3 );
     }
 
-    public function email_add_footer( $message, $slug, $args ) {
-        
+    public function email_add_header_footer( $message, $slug, $args ) {
+
+        $lines = array_map( 'trim', explode( "\n", $message ));
+        $new_lines = array();
+
+        $header_footer_pre_html  = UM()->options()->get( 'email_templates_header_footer_pre_html' );
         $header_footer_post_html = UM()->options()->get( 'email_templates_header_footer_post_html' );
 
-        $message = str_replace( array( '</body>', '</html>' ) , '', $message );
-        $message .= html_entity_decode( wp_unslash( $header_footer_post_html ),  ENT_QUOTES, 'UTF-8' ) . '</body></html>';
+        foreach( $lines as $line ) {
+            if ( ! empty( $line )) {
+            
+                switch ( substr( $line, 0, 6 )) {
 
-        return $message;
-    }
+                    case  '<body ': $new_lines[] = $line;
+                                    $new_lines[] = html_entity_decode( wp_unslash( $header_footer_pre_html ),  ENT_QUOTES, 'UTF-8' );
+                                    break;
 
-    public function email_add_header( $message, $slug, $args ) {
+                    case  '</body': $new_lines[] = html_entity_decode( wp_unslash( $header_footer_post_html ),  ENT_QUOTES, 'UTF-8' );
+                                    $new_lines[] = $line;
+                                    break;
 
-        $header_footer_pre_html = UM()->options()->get( 'email_templates_header_footer_pre_html' );
-        $message .= '>' . html_entity_decode( wp_unslash( $header_footer_pre_html ),  ENT_QUOTES, 'UTF-8' ) . '<p></p';
+                    default:        $new_lines[] = $line;
+                }
+            }
+        }
 
+        $message = implode( "\n", $new_lines );
         return $message;
     }
 
     public function um_settings_structure_header_footer( $settings_structure ) {
 
         $settings_structure['email']['form_sections']['header_footer']['title']       = __( 'Header/Footer', 'ultimate-member' );
-        $settings_structure['email']['form_sections']['header_footer']['description'] = __( 'Plugin version 1.2.0 - tested with UM 2.8.4', 'ultimate-member' );
+        $settings_structure['email']['form_sections']['header_footer']['description'] = __( 'Plugin version 1.3.0 - tested with UM 2.8.4', 'ultimate-member' );
 
         $settings_structure['email']['form_sections']['header_footer']['fields'][] =
  
                 array(
                     'id'          => 'email_templates_header_footer_pre_html',
                     'type'        => 'wp_editor',
-                    'label'       => __( 'Custom Header', 'ultimate-member' ),
-                    'description' => __( 'Enter your HTML Email Header', 'ultimate-member' ),
+                    'label'       => __( 'Custom Email Header', 'ultimate-member' ),
+                    'description' => __( 'Enter your HTML Email Header for all outgoing UM Notification emails.', 'ultimate-member' ),
                 );
 
         $settings_structure['email']['form_sections']['header_footer']['fields'][] =
@@ -62,8 +73,8 @@ class UM_Email_Header_Footer {
                 array(
                     'id'          => 'email_templates_header_footer_post_html',
                     'type'        => 'wp_editor',
-                    'label'       => __( 'Custom Footer', 'ultimate-member' ),
-                    'description' => __( 'Enter your HTML Email Footer', 'ultimate-member' ),
+                    'label'       => __( 'Custom Email Footer', 'ultimate-member' ),
+                    'description' => __( 'Enter your HTML Email Footer for all outgoing UM Notification emails.', 'ultimate-member' ),
                 );
 
         return $settings_structure;
